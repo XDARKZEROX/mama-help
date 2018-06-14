89a2402a9920d7e25f6888ea1a5ee816ac986074
Alexander Guzman <nocturnal.classical@hotmail.com>
2018-06-14 18:07:37 -0500

    Agregado segunda activity, agregada validacion de login

--- app/src/main/java/com/cibertec/mamahelp/MainActivity.kt
+++ app/src/main/java/com/cibertec/mamahelp/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.cibertec.mamahelp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TextInputLayout
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        print("test")
         btn_login_submit.setOnClickListener(this)
-
     }
 
     override fun onClick(view: View?) {
@@ -27,8 +30,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun signIn(email: String, password: String){
-        print(email)
-        print(password)
+
+        if(email.equals("admin@hotmail.com",true) && password.equals("test",true)){
+            Toast.makeText(applicationContext, "Login successfull", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        } else {
+            alert ("El correo o clave que ingresaste son incorrectos") {
+                title = "Mama Help says:"
+                yesButton { }
+                noButton { }
+            }.show()
+        }
     }
 
 }
